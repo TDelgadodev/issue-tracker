920861c856c211f56da13dcf5e9bb9d759d17c08
Thiago <thiagodelgado.dev@gmail.com>
2024-04-13 19:47:49 -0300

    implement filter to the issues list

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,39 +1,102 @@
 "use client";
 
+import Skeleton from "@/app/components/Skeleton";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
-import classNames from "classnames";
+import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
-const Navbar = () => {
+const NavBar = () => {
+  return (
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug />
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+const NavLinks = () => {
   const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
+    { label: "Issues", href: "/issues/list" },
   ];
+
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
           <Link
-            key={link.href}
-            className={classNames({
-              'text-zinc-900': link.href === currentPath,
-              'text-zinc-500' : link.href !== currentPath,
-              'hover: text-zinc-800 transition-colors': true,
+            className={classnames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
             })}
             href={link.href}
           >
             {link.label}
           </Link>
-        ))}
-      </ul>
-    </nav>
+        </li>
+      ))}
+    </ul>
   );
 };
 
-export default Navbar;
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return <Skeleton width="3rem" />;
+
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Log out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+export default NavBar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import "@radix-ui/themes/styles.css";
-import './theme-config.css';
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
 import Navbar from "./components/Navbar";
+import AuthProvider from "./auth/Provider";
 
 const inter = Inter({
-   subsets: ["latin"],
-   variable: '--font-inter', 
+  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -24,10 +25,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme appearance="light" accentColor="violet" grayColor="gray">
-          <Navbar />
-          <main className="p-5">{children}</main>
-        </Theme>
+        <AuthProvider>
+          <Theme appearance="light" accentColor="violet" grayColor="gray">
+            <Navbar />
+            <main className="p-5">{children}</main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
